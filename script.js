e16c54e3639e09178039275c0afadc33486cffb0
John Carges <johncarges@gmail.com>
2023-06-21 10:17:59 -0400

    adds mouseover functionality to main header

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+
+// Adds second header programatically
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "But this stuff over here looks better";
 
 document.querySelector("body").appendChild(h2);
+
+// Mouseover event - change header color
+const mainHeader = document.querySelector("h1");
+mainHeader.addEventListener(
+    "mouseenter",
+    (event) => {
+        event.target.style.color = "purple";
+
+        /* setTimeout(() => {
+            event.target.style.color = "";
+        }, 500); */
+    }
+)
+
+mainHeader.addEventListener(
+    "mouseleave",
+    (event) => {
+        event.target.style.color = "";
+
+        /* setTimeout(() => {
+            event.target.style.color = "";
+        }, 500); */
+    }
+)
